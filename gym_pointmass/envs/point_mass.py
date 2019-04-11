fd9fdce2441c36355af5bc01a809cc64e51069dd
justinvyu <justin.v.yu@gmail.com>
2019-04-11 16:50:16 -0700

    Add clipping at edges

--- gym_pointmass/envs/point_mass.py
+++ gym_pointmass/envs/point_mass.py
@@ -8,15 +8,26 @@ from gym.spaces import Box
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from enum import Enum
+
+class PointMassEnvRewardType(Enum):
+    DISTANCE = 1
+    SPARSE = 2
 
 class PointMassEnv(MultitaskEnv):
+    """Implementation of a 2D PointMass environment."""
+
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, dimension=2, n=1):
+    def __init__(self,
+                 dimension=2,
+                 n=1,
+                 reward_type=PointMassEnvRewardType.DISTANCE):
         MultitaskEnv.__init__(self)
 
         self.dimension = dimension
         self.num_goals = n
+        self.reward_type = reward_type
 
         # Observation is a 2D vector (the x/y coordinates of the agent)
         self.observation_space = Box(low=-1, high=1, shape=(dimension,), dtype=np.float32)
@@ -51,14 +62,46 @@ class PointMassEnv(MultitaskEnv):
     def _get_info(self):
         pass
 
+    def _move_agent(self, action):
+        """Expects an 2D action vector.
+        >>> e = PointMassEnv()
+        >>> e.agent_position = np.array([-1., 0.])
+        >>> ne, se, sw, nw = np.array([1, 1]), np.array([1, -1]), np.array([-1, -1]), np.array([-1, 1])
+        >>> print(e._move_agent(2 * ne))
+        [0. 1.]
+        >>> print(e._move_agent(2 * se))
+        [1. 0.]
+        >>> print(e._move_agent(2 * sw))
+        [ 0. -1.]
+        >>> print(e._move_agent(2 * nw))
+        [-1.  0.]
+        """
+        assert len(action) == self.dimension, "Dimension of action does not align, expected" \
+                                              " ({0},) but got {1}".format(self.dimension, action.shape)
+        new_pos = self.agent_position + action
+        new_x, new_y = new_pos[0], new_pos[1]
+        old_x, old_y = self.agent_position[0], self.agent_position[1]
+        act_x, act_y = action[0], action[1]
+
+        # If x/y goes out of bounds [-1, 1]:
+        if np.abs(new_y) > 1 and np.abs(new_y - old_y) >= np.abs(new_x - old_x):
+            scale = (1 - np.abs(old_y)) / np.abs(new_y - old_y)
+            new_x = old_x + scale * act_x
+            new_y = 1.0 if new_y > 1 else -1
+        elif np.abs(new_x) > 1:
+            scale = (1 - np.abs(old_x)) / np.abs(new_x - old_x)
+            new_y = old_y + scale * act_y
+            new_x = 1.0 if new_x > 1 else -1
+
+        self.agent_position = np.array([new_x, new_y])
+        return self.agent_position
+
     def step(self, action):
         """Adds action, which should be a n-dimensional vector, to the current agent."""
         action = np.array(action)
-        assert len(action) == self.dimension, "Dimension of action does not align, expected" \
-                                              " ({0},) but got {1}".format(self.dimension, action.shape)
 
         # Move the agent.
-        self.agent_position += action
+        self._move_agent(action)
 
         obs = self._get_observation()
         reward = self.compute_reward(action, obs)
@@ -105,3 +148,25 @@ class PointMassEnv(MultitaskEnv):
         r = -np.linalg.norm(achieved_goals - desired_goals, axis=1)
 
         return r
+
+if __name__ == "__main__":
+    # env = PointMassEnv()
+    # env.reset()
+    # env.agent_position = np.array([0, 0])
+    # env.step(np.array([0.5, 0.5]))
+    # print(env.agent_position)
+    # env.step(np.array([0.2, 0.1]))
+    # print(env.agent_position)
+    # env.step(np.array([1.0, 1.0]))
+    # print(env.agent_position)
+    # env.step(np.array([0, 100.0]))
+    # print(env.agent_position)
+
+    env2 = PointMassEnv()
+    env2.reset()
+    env2.agent_position = np.array([-1., 0.])
+    env2.step(np.array([2, 2]))
+    print(env2.agent_position)
+    env2.step(np.array([2, -2]))
+    print(env2.agent_position)
+
